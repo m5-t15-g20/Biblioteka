c717bdbe3bf6f08fc578c8794cced2f47c507d75
gabriel <gabriel.c8944@gmail.com>
2023-07-04 13:28:16 -0300

    feat: patching book bugs solved

--- books/models.py
+++ books/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=255, null=False)
+    title = models.CharField(max_length=255, null=False, unique=True)
     author = models.CharField(max_length=50, null=False)
     sinopse = models.TextField(null=True, default=None)
     coverImage = models.CharField(max_length=50, null=True, default=None)
