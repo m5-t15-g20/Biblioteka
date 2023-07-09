eec481a4458099e2b9298dca3193b0fb7f830433
Gael <rchsilver@gmail.com>
2023-07-09 09:39:09 -0300

    fix: choices in user

--- users/models.py
+++ users/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+class User_choices(models.TextChoices):
+    student = "Student"
+    library_collaborator = "Library Collaborator"
 
 class User(AbstractUser):
-    user_choices = (
-        ("Student", "Student"),
-        ("Library Collaborator", "Library Collaborator"),
-    )
+    # user_choices = (
+    #     ("Student", "Student"),
+    #     ("Library Collaborator", "Library Collaborator"),
+    # )
 
     email = models.EmailField(
         unique=True, error_messages={"unique": "This field must be unique."}
     )
     is_authorized = models.BooleanField(default=True)
     user_type = models.CharField(
-        max_length=20, choices=user_choices, default="Student", blank=True, null=True
+        max_length=20, choices=User_choices.choices, default=User_choices.student, blank=True, null=True
     )
     following = models.ManyToManyField("books.Book", related_name="followers")
-
-
-
